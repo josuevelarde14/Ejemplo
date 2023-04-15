6884b2a55468c9fe31fd82944a8bd006c937e976
Josue Velarde <129917394+josuevelarde14@users.noreply.github.com>
2023-04-14 20:30:11 -0500

    Mat python C2

---
+++ Clase 3 python/Ejemplo.py
@@ -0,0 +1,18 @@
+a=10
+b=3
+print(a + b) # Suma: 13
+print(a - b) # Resta: 7
+print(a * b) # Multiplicación:
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
+c=7
+d=1
+e=6
+f=5
+g=4
+print(c-d*e+f/g)
+print(7-1*6+5/4)
+
+
