6f749612d2b9fdd2b4a9eb6f533ebefcfd72be67
josuevelarde14 <129917394+josuevelarde14@users.noreply.github.com>
2023-04-21 17:29:08 -0500

    Avance python

---
+++ Clase 4 python/S2 Estrc control de flujo/Ej1.py
@@ -0,0 +1,13 @@
+# Ejemplo de estructura de control de flujo
+edad = 18
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+i = 0 
+while i < 5:
+    print(i)
+    i += 1
+for letra in "Python":
+    print(letra)
